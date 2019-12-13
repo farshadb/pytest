a93d6688a642a3405a238af926619f5552904389
Farshad Bolouri <bolouri.farshad@gmail.com>
2019-12-13 23:54:13 +0330

    Initial Commit for adding existing folder to gitlab

--- path-pathlib.py
+++ path-pathlib.py
@@ -1,8 +1,17 @@
 from pathlib import Path
 
 
-path = Path("ecommerce")
-print(path.exists())
+# path = Path("ecommerce")
+# print(path.exists())
+#
+# # path = Path("emails")
+# # print(path.rmdir())
+# #path.mkdir() will create a new path in our project folder
+# path = Path("scripts")
+# print(path.mkdir())
+# print(path.glob("*.* or *.xml or ....))
 
-path = Path("emails")
-print(path.rmdir())
+path = Path("")
+print(path)
+for file in path.glob("*"):
+    print(file)
